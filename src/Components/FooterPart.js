c2c89b55cc7e39dce6081a4dbf139be89b0a4a2b
kishorkc <kishor8007kc@gmail.com>
2021-12-10 22:29:28 +0545

    change footer

--- src/Components/FooterPart.js
+++ src/Components/FooterPart.js
@@ -6,7 +6,7 @@ export default function FooterPart() {
     <>
       <Box sx={{}} component="footer">
         <Typography variant="h6" align="center">
-          Footer
+          ----
         </Typography>
         <Typography
           variant="subtitle1"
@@ -14,7 +14,7 @@ export default function FooterPart() {
           color="text.secondary"
           component="p"
         >
-          Something here to give the footer a purpose!
+          Keep Learning - Keep Coding
         </Typography>
         <Typography variant="body2" color="text.secondary" align="center">
           {"Copyright © "}
